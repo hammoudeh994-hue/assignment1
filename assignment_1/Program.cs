5a04a9054a895be33d08f3f9260d671738c0d838
nosno khader <hammoudeh994@gmail.com>
2026-02-14 22:37:05 -0800

    new code

--- assignment_1/Program.cs
+++ assignment_1/Program.cs
@@ -1,2 +1,29 @@
-﻿Console.WriteLine("Heloo.World");
+﻿Console.WriteLine("enter the invoice amount");
+double amount = Convert.ToDouble(Console.ReadLine());
 
+
+if (amount <= 0)
+{
+    Console.WriteLine("Invalid amount :{amount}");
+
+
+
+}
+else if (amount >= 500)
+{
+    double discountAmount = amount * 0.2;
+    double finalAamount = amount - discountAmount;
+    Console.WriteLine($"The final amount after discount (20%) : {finalAamount}");
+  }
+
+else if (amount < 500 && amount >= 300)
+{
+    double discountAmount = amount * 0.1;
+    double finalAamount = amount - discountAmount;
+    Console.WriteLine($"The final amount after discount (10%) : {finalAamount}");
+}
+
+else
+{
+    Console.WriteLine($"The final amount : {amount}");
+}
